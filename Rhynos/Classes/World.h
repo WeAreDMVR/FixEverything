d214485239e9431e5eedfa4755c959a07ed515a8
Lee Ehudin <percivalgambit@gmail.com>
2017-05-04 14:31:26 -0500

    More physics fiddling

--- Rhynos/Classes/World.h
+++ Rhynos/Classes/World.h
@@ -3,9 +3,9 @@
 
 #include <Box2D/Box2D.h>
 
-constexpr float TimeStep = 1 / 60.0;
-constexpr float PixelsPerMeter = 35.0;
-constexpr float Gravity = -20;
+constexpr float TimeStep = 1 / 120.0;
+constexpr float PixelsPerMeter = 75.0;
+constexpr float Gravity = -9.8;
 constexpr float Friction = 0.06;
 constexpr float Density = 1.0;
 constexpr float Restitution = 0.0;
