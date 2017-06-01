acfa544d2f468a29558b4437d0d3eb7e71fe0fca
Long Pham <longpham@uchicago.edu>
2017-06-01 01:15:35 -0500

    cleaned up

--- Rhynos/Classes/AI.cpp
+++ Rhynos/Classes/AI.cpp
@@ -35,6 +35,7 @@ int AI::move() {
     return 1;
   }
 
+
   // Check if we going to mess up
   if (prob < 0) {
     return 0;
@@ -69,52 +70,57 @@ void AI::analyzeMap(const TMXTiledMap* map) {
     auto y_pos = this->getPositionY();
     auto x_pos = this->getPositionX();
     
-    CCLOG("PREV");
-    CCLOG( "%.2f, %.2f", this->prev.x, this->prev.y);
-    CCLOG("NEW POS");
-    CCLOG( "%.2f, %.2f", x_pos, y_pos);
-    
     auto pt = tileCoordForPosition(Point(x_pos, y_pos));
     auto currLayer = this->getLayerNum();
     
-    if (this->isOffMap()) {
+    // Check for out of bounds
+    if (this->isOffMap() || pt.x > mapSize.width - 5 || pt.y > mapSize.height - 5) {
         return;
     }
     
     for (int j= currLayer; j < currLayer + 3; j++) {
+        // Cycle through the three layer starting at the current one
         int i = (j % 3);
         if (i==0) {
             i = 3;
         }
+
         TMXLayer* layer1 = map->getLayer("meta" + to_string(i));
     
+    	// Start at top right then go to bottom left
         for (int y = pt.y+5; y > pt.y; y--) {
             for (int x = pt.x+5; x > pt.x; x--) {
                 Sprite* tileSprite = layer1->getTileAt(pt);
-            
-                if (tileSprite) {
+
+            	// Test where its starting
+            	layer1->removeChild(tileSprite);
+                if (true) {
                     tileSprite->setAnchorPoint(Point(0.5, 0.5));
+                    
                     const int tileGID = layer1->getTileGIDAt(pt);
-                    const cocos2d::ValueMap properties =
-                    map->getPropertiesForGID(tileGID).asValueMap();
+                    const cocos2d::ValueMap properties = map->getPropertiesForGID(tileGID).asValueMap();
                     cocos2d::ValueMap* ptr_properties = new ValueMap(properties);
                 
+
+
+ 					// The next tile is there on this layer so just keep going straight	               
                     if (x == pt.x+1 && y == pt.y+1 && tileGID !=0) {
                         CCLOG("straight");
+                        this->applyMoveRight();
+                        this->setLayer(i);
+                        goto action;
+                    }
+                    
+                    // Jumping will get to another tile
+                    if (x > pt.x + 2 && tileGID != 0) {
+                        CCLOG("here");
+                        this->applyJump();
                         this->applyMoveRight();
                         goto action;
                     }
                     
-                    if (x > pt.x + 2 && tileGID != 0) {
-                        CCLOG("here");
-                        this->applyJump();
-                        goto action;
-                    }
-                    
-                    
-                    
-                    
-                    if (tileGID == 0 && x < pt.x + 3) {
+                    // There's a straight line so u can just jump
+                    if (tileGID == 0 && x < pt.x + 3 && y < pt.y+2) {
                         this->applyJump();
                         this->setLayer(i);
                         goto action;
